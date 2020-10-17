baa5da23a3d8b4639b1a0ee2a04d7a718e3d3023
MarkoPolo618 <czarek.wator@onet.pl>
2020-10-17 15:06:48 +0200

    Wstępne opracowanie widoku i kontrolera LimitsActivity

--- app/src/main/java/com/jrcw/expensemonitor/LimitsActivity.java
+++ app/src/main/java/com/jrcw/expensemonitor/LimitsActivity.java
@@ -3,6 +3,10 @@ package com.jrcw.expensemonitor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class LimitsActivity extends AppCompatActivity {
 
@@ -11,4 +15,69 @@ public class LimitsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_limits);
     }
+
+    public Button getButtonByName(String name) throws Exception {
+        Button btn = null;
+        switch (name) {
+            case "Szczegoly":
+                btn = (Button) this.findViewById(R.id.button);
+                break;
+            case "Edytuj":
+                btn = (Button) this.findViewById(R.id.btnEdycja);
+                break;
+            case "Ustaw":
+                btn = (Button) this.findViewById(R.id.btnUstaw);
+                break;
+            case "Kategorie":
+                btn = (Button) this.findViewById(R.id.btnKategorie);
+                break;
+            case "Produkty":
+                btn = (Button) this.findViewById(R.id.btnProdukty);
+                break;
+        }
+        if (btn != null) {
+            return btn;
+        } else {
+            throw new Exception();
+        }
+    }
+
+    public EditText getEditTextByName(String name) throws Exception {
+        EditText txt = null;
+        switch (name) {
+            case "Fundusze":
+                txt = (EditText) this.findViewById(R.id.editTextFundusze);
+                break;
+            case "Sztuki":
+                txt = (EditText) this.findViewById(R.id.editTextSztuki);
+                break;
+        }
+        if (txt != null) {
+            return txt;
+        } else {
+            throw new Exception();
+        }
+    }
+
+    public SeekBar getSeekBarByName(String name) throws Exception {
+        SeekBar sb = null;
+        switch (name) {
+            case "Fundusze":
+                sb = (SeekBar) this.findViewById(R.id.seekFundusze);
+                break;
+
+            case "Sztuki":
+                sb = (SeekBar) this.findViewById(R.id.seekSztuki);
+                break;
+        }
+        if (sb != null) {
+            return sb;
+        } else {
+            throw new Exception();
+        }
+
+    }
 }
+
+
+
--- app/src/main/java/com/jrcw/expensemonitor/LimitsActivityController.java
+++ app/src/main/java/com/jrcw/expensemonitor/LimitsActivityController.java
@@ -1,4 +1,131 @@
 package com.jrcw.expensemonitor;
 
+import android.app.SearchManager;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.SeekBar;
+
 public class LimitsActivityController {
+
+    private LimitsActivity view;
+    private LimitsActivityModel model;
+
+
+    public LimitsActivityController(LimitsActivity view, LimitsActivityModel model) {
+        this.view = view;
+        this.model = model;
+        initView();
+    }
+
+    private void initView() {
+        try {
+            view.getSeekBarByName("Fundusze").setMax(1000);
+            view.getSeekBarByName("Sztuki").setMax(100);
+            view.getEditTextByName("Fundusze").setText("0");
+            view.getEditTextByName("Sztuki").setText("0");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private class KategorieOnClickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+    private class ProduktyOnClickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+
+    private class UstawOnClickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+
+    private class EdytujOnClickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+    private class SztukiTextChangedListener implements TextWatcher {
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    }
+    private class FunduszeTextChangedListener implements  TextWatcher {
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    }
+    private class FunduszeSeekBarListener implements SeekBar.OnSeekBarChangeListener {
+
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    }
+    private class SztukiSeekBarListener implements SeekBar.OnSeekBarChangeListener {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    }
 }
